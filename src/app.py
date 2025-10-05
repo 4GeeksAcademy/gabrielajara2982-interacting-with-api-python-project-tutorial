e343eea4e18ebd5522899b102f98e6654c10438f
Gaby Jara <gabyjargudo82@gmail.com>
2025-10-05 15:35:14 +0000

    API de Spotify

--- src/app.py
+++ src/app.py
@@ -1,10 +1,47 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
+
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+artist_id = "6HqPNOo6OV9rPbEY7MP9T8"
+
+# Conseguir las canciones top del artista
+results = spotify.artist_top_tracks(artist_id)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
+print(songs)
+
+# se hace DF
+tracks_df = pd.DataFrame(songs)
+
+print(tracks_df.head(11))
+
+# Visualizacion de Datos
+
+plt.scatter(tracks_df['duration_min'], tracks_df['popularity'])
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Popularity')
+plt.title('Relationship between duration and popularity')
+plt.show()
+
